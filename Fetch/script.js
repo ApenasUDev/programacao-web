33dd011f86c813f9d6006fb8df7646c3a9cf0b31
ApenasUDev <francimaralexandredt@gmail.com>
2023-08-26 15:07:38 -0300

    finalmente faltava um ?size=3 na api de bancos

--- Fetch/script.js
+++ Fetch/script.js
@@ -1,69 +1,45 @@
+let cervejas = [];
+let bancos = [];
 
-
-let cervejas = []
-
-let bancos = []
-
-//cs é um array de cervejas
-
+// cs é um array de cervejas
 const carregarDiv = cs => {
+    const div = document.getElementById("cervejasDiv");
+    const itensHtml = cs.map(({ name, alcohol, blg, brand, hop, ibu, id, malts, style, uid, yeast }) =>
+        `<div>${name} -- ${alcohol} -- ${blg} -- ${brand} -- ${hop} -- ${ibu} -- ${id} -- ${malts} -- ${style} -- ${uid} -- ${yeast}</div>`
+    );
+    div.innerHTML = itensHtml.join("\n");
+};
 
-    const div = document.getElementById("cervejasDiv")
-
-
-    const itensHtml = cs.map(({ name, alcohol, blg, brand, hop, ibu, id, malts, style, uid, yeast }) => `<div>${name} -- ${alcohol} -- ${blg} -- ${brand} -- ${hop} -- ${ibu} -- ${id} -- ${malts} -- ${style} -- ${uid} -- ${yeast}</div>`)
-
-    div.innerHTML = `${itensHtml.join("\n")}`
-
-}
 const carregarDivBancos = cs => {
-    const div = document.getElementById("cervejasDiv")
-
-
-    const itensHtml = cs.map(({ account_number, bank_name, iban, id, routing_number, swift_bic, uid }) => `<div>${bank_name} -- ${account_number} -- ${iban} -- ${routing_number} -- ${swift_bic} -- ${uid} -- ${id} </div>`)
-
-    div.innerHTML = `${itensHtml.join("\n")}`
-}
-
-
+    const div = document.getElementById("cervejasDiv");
+    const itensHtml = cs.map(({ account_number, bank_name, iban, id, routing_number, swift_bic, uid }) =>
+        `<div>${bank_name} -- ${account_number} -- ${iban} -- ${routing_number} -- ${swift_bic} -- ${uid} -- ${id}</div>`
+    );
+    div.innerHTML = itensHtml.join("\n");
+};
 
 async function carregarBancos() {
     try {
-        let res = await fetch("https://random-data-api.com/api/v2/banks")
-
-        bancos = await res.json()
-
-        carregarDivBancos(bancos)
+        let res = await fetch("https://random-data-api.com/api/v2/banks?size=3");
+        bancos = await res.json();
+        carregarDivBancos(bancos);
     } catch (err) {
-        document.getElementById("cervejasDiv").innerHTML = "Fudeu..."
-
+        document.getElementById("cervejasDiv").innerHTML = "Fudeu...";
     }
 }
 
 async function carregarCervejas() {
-
     try {
-
-        let res = await fetch("https://random-data-api.com/api/v2/beers?size=3")
-
-        cervejas = await res.json()
-
-        carregarDiv(cervejas)
-
+        let res = await fetch("https://random-data-api.com/api/v2/beers?size=3");
+        cervejas = await res.json();
+        carregarDiv(cervejas);
     } catch (err) {
-
-        document.getElementById("cervejasDiv").innerHTML = "Fudeu..."
-
+        document.getElementById("cervejasDiv").innerHTML = "Fudeu...";
     }
 }
 
+let botao = document.getElementById("botaoCarregar");
+let botao2 = document.getElementById("botaoCarregarBancos");
 
-
-
-
-let botao = document.getElementById("botaoCarregar")
-let botao2 = document.getElementById("botaoCarregarBancos")
-
-botao.addEventListener("click", () => carregarCervejas())
-botao2.addEventListener("click", () => carregarBancos())
-
+botao.addEventListener("click", carregarCervejas);
+botao2.addEventListener("click", carregarBancos);
