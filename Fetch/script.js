431657c2426fc05a59064a2d77dc59771f0e3540
ApenasUDev <francimaralexandredt@gmail.com>
2023-08-28 09:22:07 -0300

    update

--- Fetch/script.js
+++ Fetch/script.js
@@ -1,17 +1,7 @@
 // script.js
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("botaoCarregar").addEventListener("click", async () => {
-        try {
-            const response = await fetch("https://api.punkapi.com/v2/beers");
-            const data = await response.json();
-            const cervejasDiv = document.getElementById("cervejasDiv");
-            cervejasDiv.innerHTML = "";
-            cervejasDiv.appendChild(generateTable(data));
-        } catch (error) {
-            console.error("Erro ao carregar cervejas:", error);
-        }
-    });
+
 
     document.getElementById("botaoCarregarBancos").addEventListener("click", async () => {
         try {
